9ac66a39e0c751fd5c2a29e44d4e73e45e010dcd
skf-project <74064182+skf-project@users.noreply.github.com>
2023-09-23 23:31:57 +0530

    Added Assertion

--- src/test/java/com/lti/testcases/VehicleInsurance.java
+++ src/test/java/com/lti/testcases/VehicleInsurance.java
@@ -1,6 +1,7 @@
 package com.lti.testcases;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.lti.base.TestBase;
@@ -8,9 +9,17 @@ import com.lti.base.TestBase;
 public class VehicleInsurance extends TestBase {
 
 	@Test
-	public void loginHome()
+	public void loginHome() throws InterruptedException
 	{
 		driver.findElement(By.id(OR.getProperty("motorcylelink"))).click();
 		log.debug("Clicked on motorcycle link");
+		String title=driver.getTitle();
+		System.out.println(title);
+		Assert.assertEquals(title, "Enter Vehicle Data");
+		Thread.sleep(3000);
+		String insuranceType=driver.findElement(By.id(OR.getProperty("insuranceType"))).getText();
+		Assert.assertEquals(insuranceType, "Motorcycle Insurance");
+		Assert.assertTrue(driver.findElement(By.id(OR.getProperty("insuranceType"))).isDisplayed());
+		
 	}
 }
